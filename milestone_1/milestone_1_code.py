9a5eab1f817125e947652513cdea8741ba6d8ccd
JalenGreen2009 <zhouj@wsc.school.nz>
2025-06-17 14:16:54 +1200

    update code

--- milestone_1/milestone_1_code.py
+++ milestone_1/milestone_1_code.py
@@ -1,4 +1,10 @@
 feel = input("How are you?")
-print('you feel '+ feel)
+print('you feel '+ feel) 
+if feel == ("good"):
+    print("Wonderful,then next question")
+if feel == ("bad"):
+    print("Sorry to hear that")
 FOOD = input("What did you eat")
 print(" your food "+ FOOD )
+Thing = ('What are you doing')
+print("the Thing you doing"+ things)
